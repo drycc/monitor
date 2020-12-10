ce8c69775b59e47fc769f263d06ca3a197c684da
duanhongyi <duanhongyi@doopai.com>
2020-12-10 15:51:36 +0800

    chore(monitor): remove docker keyword from charts

--- deploy.mk
+++ deploy.mk
@@ -1,9 +1,9 @@
 install:
-	helm upgrade monitor ../charts/monitor --install --namespace drycc --set grafana.org=${IMAGE_PREFIX},grafana.docker_tag=${VERSION},telegraf.org=${IMAGE_PREFIX},telegraf.docker_tag=${VERSION}
+	helm upgrade monitor ../charts/monitor --install --namespace drycc --set grafana.org=${IMAGE_PREFIX},grafana.image_tag=${VERSION},telegraf.org=${IMAGE_PREFIX},telegraf.image_tag=${VERSION}
 
 
 upgrade: 
-	helm upgrade monitor ../charts/monitor --namespace drycc --set grafana.org=${IMAGE_PREFIX},grafana.docker_tag=${VERSION},telegraf.org=${IMAGE_PREFIX},telegraf.docker_tag=${VERSION}
+	helm upgrade monitor ../charts/monitor --namespace drycc --set grafana.org=${IMAGE_PREFIX},grafana.image_tag=${VERSION},telegraf.org=${IMAGE_PREFIX},telegraf.image_tag=${VERSION}
 
 uninstall:
 	helm delete monitor
